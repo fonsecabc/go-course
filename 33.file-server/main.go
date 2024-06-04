8b338af4f8a584f6dc33418ae975c76613ec9146
fonsecabc <caiobragadafonseca@gmail.com>
2024-06-04 16:38:32 -0300

    fix: main.go 33

--- 33.file-server/main.go
+++ 33.file-server/main.go
@@ -4,5 +4,10 @@ import "net/http"
 
 const port = ":8080"
 
+func main() {
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./public"))
 
+	mux.Handle("/", fileServer)
+	http.ListenAndServe(port, mux)
 }
